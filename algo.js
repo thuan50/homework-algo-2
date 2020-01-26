727881885c53602842249ba11dacb60d3bd96f5f
thuan50 <59578561+thuan50@users.noreply.github.com>
2020-01-26 11:36:45 -0800

    Add files via upload

---
+++ algo.js
@@ -0,0 +1,14 @@
+//Homeowrk Algo Hw2 Question 1 Jan 26
+
+function power (8,4){
+    return Math.pow (8,4);
+}
+console.log (power (4));
+
+//Homework Algo Hw2 Question 2
+
+function accept (4 ){
+    return Math.random ( 4);
+
+}
+console.log (random (4));
